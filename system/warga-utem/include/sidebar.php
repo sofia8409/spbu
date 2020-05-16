fd1fa39370f30bf7affa52c7b2784b9419711760
sofia8409 <65439175+sofia8409@users.noreply.github.com>
2020-05-16 15:35:45 +0800

    Add files via upload

---
+++ system/warga-utem/include/sidebar.php
@@ -0,0 +1,78 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>NAVIGASI UTAMA</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard-WU.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Laman Utama </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							
+							
+							<li>
+								<a href="dashboard-WU.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Permohonan </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="senaraiharga-WU.php">
+											<span class="title"> Senarai Harga </span>
+										</a>
+									</li>
+									
+									<li>
+										<a href="senarai-permohonanWU.php">
+											<span class="title"> Senarai Permohonan</span>
+										</a>
+									</li>
+									
+									<li>
+										<a href="pembayaran.php">
+											<span class="title"> Pembayaran </span>
+										</a>
+									</li>
+							</ul>
+							
+						<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-bell"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Pemberitahuan </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							
+							
+		
+								
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
