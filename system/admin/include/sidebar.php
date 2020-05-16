92e9677129f2da7fde3bd87fc5eab3837b2a6bc1
sofia8409 <65439175+sofia8409@users.noreply.github.com>
2020-05-16 15:21:31 +0800

    Add files via upload

---
+++ system/admin/include/sidebar.php
@@ -0,0 +1,142 @@
+<div class="sidebar app-aside" id="sidebar">
+				<div class="sidebar-container perfect-scrollbar">
+
+<nav>
+						
+						<!-- start: MAIN NAVIGATION MENU -->
+						<div class="navbar-title">
+							<span>NAVIGASI UTAMA</span>
+						</div>
+						<ul class="main-navigation-menu">
+							<li>
+								<a href="dashboard-admin.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-home"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Laman Utama </span>
+										</div>
+									</div>
+								</a>
+							</li>
+							<li>
+								<a href="javascript:void(0)">
+								<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="material-icons">cached</i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Kemaskini </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									<li>
+										<a href="kemaskini-bas.php">
+											<span class="title">  Bas </span>
+										</a>
+									</li>
+									<li>
+										<a href="kemaskini-pemandu.php">
+											<span class="title">  Pemandu</span>
+										</a>
+									</li>
+									<li>
+										<a href="kemaskini-cukai.php">
+											<span class="title">  Cukai</span>
+										</a>
+									</li>
+									<li>
+										<a href="kemaskini-kadarsewaanWU.php">
+											<span class="title">  Kadar Sewaan Warga UTeM</span>
+										</a>
+									</li>
+									<li>
+										<a href="kemaskini-kadarsewaanPL.php">
+											<span class="title">  Kadar Sewaan Pihak Luar</span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-file"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Permohonan </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="senaraiharga-wargaUTeM.php">
+											<span class="title"> Senarai Harga Warga UTeM </span>
+										</a>
+									</li>
+									<li>
+										<a href="senaraiharga-PL.php">
+											<span class="title"> Senarai Harga Pihak Luar</span>
+										</a>
+									</li>
+									
+									<li>
+										<a href="senarai-tempahan.php">
+											<span class="title"> Senarai Permohonan </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								
+
+								<li>
+									<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-bell"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Pemberitahuan</span>
+										</div>
+									</div>
+								</a>
+								</li>
+								<li>
+								<a href="appointment-history.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="fa fa-bar-chart"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Laporan </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								<li>
+								<a href="patient-search.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-search"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Carian Pengguna </span>
+										</div>
+									</div>
+								</a>
+							</li>
+								</li>
+
+						</ul>
+						<!-- end: CORE FEATURES -->
+						
+					</nav>
+					</div>
+			</div>
